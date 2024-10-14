eda3dba40492ee9fba2f581d407b752827aec5d4
Sebaescu <sebaescu@espol.edu.ec>
2024-10-13 22:05:39 -0500

    Se añadio la foto y el menu principal

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 L.marker([-2.144586,-79.967645]).addTo(map)
     .bindPopup('A pretty CSS popup.<br> Easily customizable.')
     .openPopup();
+    function updatemenu() {
+        if (document.getElementById('responsive-menu').checked == true) {
+          document.getElementById('menu').style.borderBottomRightRadius = '0';
+          document.getElementById('menu').style.borderBottomLeftRadius = '0';
+        }else{
+          document.getElementById('menu').style.borderRadius = '10px';
+        }
+      }
